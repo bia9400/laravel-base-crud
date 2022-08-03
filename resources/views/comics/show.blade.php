9de099b53f0fc285344b98fb32b9935906f39aa5
bia9400 <ferro.biagio@gmail.com>
2022-08-03 17:32:16 +0200

    show secion

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,7 +4,11 @@
     
 @section('page_content')
 <div class="container py-3 text-center">
-    {{$fumetto->description}}
+    <h1>{{$fumetto->title}}</h1>
+    <p>{{$fumetto->description}}</p>
+    <img src="{{$fumetto->thumb}}" alt="">
+    <h3>Prezzo: {{$fumetto->price}}$</h3>
+    
 </div>
 
 
